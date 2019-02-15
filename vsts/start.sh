eefd71223cc852d049ca66bec01762deec24eaf2
Roman Judin <RJ@vistex.com>
2019-02-15 11:23:26 +0100

    vsts: no sigterm; aws: AWScliV=1.16.105 #1.11.156

--- vsts/start.sh
+++ vsts/start.sh
@@ -6,6 +6,7 @@ export VSO_AGENT_IGNORE=_,MAIL,OLDPWD,PATH,PWD,VSTS_AGENT,VSTS_ACCOUNT,VSTS_TOKE
 if [ -z $VSTS_AGENT_VERSION ]; then
   VSTS_AGENT_VERSION=2.140.0;
 fi;
+
 echo "VSTS_AGENT_VERSION: ${VSTS_AGENT_VERSION}"
 VSTS_AGENT_URL="https://vstsagentpackage.azureedge.net/agent/${VSTS_AGENT_VERSION}/vsts-agent-linux-x64-${VSTS_AGENT_VERSION}.tar.gz"
 
@@ -13,8 +14,7 @@ echo "$VSTS_AGENT_URL"
 if [ -n "$VSTS_ACCOUNT" ]; then
   echo "START VSTS: VSTS_ACCOUNT is $VSTS_ACCOUNT"
 
-  #Instead of creating a new process, this will replace the shell process
-  exec ./start_VSTS.sh
+  RunAs=VSTS
 
 elif [ -n "$TFS_HOST" ] || [ -n "$TFS_URL" ]; then
   echo "START TFS: TFS_HOST is $TFS_HOST; TFS_URL is $TFS_URL"
@@ -22,10 +22,18 @@ elif [ -n "$TFS_HOST" ] || [ -n "$TFS_URL" ]; then
   curl -fSL $VSTS_AGENT_URL -o agent.tgz; mkdir -p agent; cd agent;
   tar -xz --no-same-owner -f ../agent.tgz; cd ..; rm agent.tgz
 
-  #Instead of creating a new process, this will replace the shell process
-  exec ./start_TFS.sh
+  RunAs=TFS
 
 else
   echo "ERORR: Not found any of VSTS_ACCOUNT, TFS_URL, TFS_HOST"
   exit -1
 fi
+
+#SIGTERM is 15 | SIGINT is 2 | SIGKILL is 9
+trap "echo 'Got SIGINT'; kill -SIGINT $PID" INT
+trap "echo 'Got SIGTERM'; kill -SIGTERM $PID" TERM
+
+exec ./start_$RunAs.sh &
+export PID=$!
+echo "Agent PID is $PID"
+# INFO: http://veithen.io/2014/11/16/sigterm-propagation.html
